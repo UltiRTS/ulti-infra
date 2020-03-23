3a253ead96e19ba9cd26bd4af907358b5af10b8b
deinBestFreund <62200071+deinBestFreund@users.noreply.github.com>
2020-03-23 08:06:02 -0400

    integrity check

--- main.py
+++ main.py
@@ -6,13 +6,19 @@ import time
 
 # Define a functions for a subserver thread
 def updaterbackend(info,info):
-   os.system(python3 ./engineupdater/runserver.py)
+   os.system("python3 ./engineupdater/runserver.py")
 
 # Threads for subserver process
-try:
-   thread.start_new_thread( print_time, (0, 0, ) )
-except:
-   print "Error: unable to start updater server backend"
-
-while 1:
-   time.sleep( 5 )
+if __name__== "__main__":
+   print("=Check Server Integrity=")
+   
+   if !os.path.exists("/opt/infra/newrelease/"):    #Integrity check for updater backends
+      os.system("mkdir /opt/infra/newrelease")
+   
+   print("=Spawning Subservers=") 
+   try:
+      thread.start_new_thread( updaterbackend, (0, 0, ) ) #launch updater backend
+   except:
+      print "Error: unable to start updater server backend"
+   while 1:
+      time.sleep( 5 )
