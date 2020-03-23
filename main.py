c0fc58ce9d75f4f8edf62d16a90cf00ef85c8bd6
deinBestFreund <62200071+deinBestFreund@users.noreply.github.com>
2020-03-23 07:47:56 -0400

    make main functional

---
+++ main.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+
+import thread
+import os
+import time
+
+# Define a functions for a subserver thread
+def updaterbackend(info,info):
+   os.system(python3 ./engineupdater/runserver.py)
+
+# Threads for subserver process
+try:
+   thread.start_new_thread( print_time, (0, 0, ) )
+except:
+   print "Error: unable to start updater server backend"
+
+while 1:
+   time.sleep( 5 )
